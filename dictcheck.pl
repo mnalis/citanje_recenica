bbbf40982567dad9f314b88c075eba3cf4e9dada
Matija Nalis <mnalis-git@voyager.hr>
2017-11-08 00:34:57 +0100

    provjera nekoristenih i pokusaja koristenja nepostojecih keyeva

--- dictcheck.pl
+++ dictcheck.pl
@@ -10,20 +10,7 @@ use Data::Dumper;
 use feature 'say';
 
 my %KEYS=();
-
-sub provjeri
-{
-	my ($key) = @_;
-	say "Parsing: $key";
-	if (!defined $KEYS{$key}) { say "ERROR: Ne postoji key $key" }
-	my @elements = split /:/, $KEYS{$key};
-	foreach my $element (@elements) {
-		say "  el=$element";
-		if ($element =~ /^[A-Z][A-Z0-9_]*$/) {	# ALL_UPERCASE => this is another key
-			provjeri($element);
-		}
-	}
-}
+my %SEEN=();
 
 ####
 #### MAIN
@@ -33,22 +20,47 @@ while (<>) {
 	next if not /^\w/;
 	chomp;
 	my ($k,$v) = split " ", $_, 2;
-	$KEYS{$k}.="$v:"
+	$KEYS{$k}.="$v:";
+	$SEEN{$k}=0;
 }
 
 
 
-say Dumper(\%KEYS);
-#provjeri ('RECENICA');
+#say Dumper(\%KEYS);
 
-
+# zamjena KEYeva sa njihovim VALUES
 # mora biti od najduzih prema najkracima kako ne bi substituirali sadrzaj od npr. RECENICA  umjesto od RECENICA_UPITNA
 foreach my $k (sort { length $b <=> length $a } keys %KEYS) {
 	my $v=$KEYS{$k};
-	say "zamjena k=$k\tv=$v";
+	#say "zamjena k=$k\tv=$v";
 	foreach my $k2 (keys %KEYS) {
-		$KEYS{$k2} =~ s{\Q$k\E}{$v}g;	# in all values, replace KEY with its VALUE
+		if ($KEYS{$k2} =~ s{\Q$k\E}{$v}g) {	# in all values, replace KEY with its VALUE
+			$SEEN{$k}++;
+		}
 	}
 }
 
-say Dumper(\%KEYS);
+#say Dumper(\%KEYS);
+#say Dumper(\%SEEN);
+
+
+my $count_errors = 0;
+
+# provjera nekoristenih KEYeva
+foreach my $k (keys %SEEN) {
+	if ($SEEN{$k} == 0 and $k ne 'RECENICA') {	# svi osim pocetnog moraju imati barem jednog childa
+		$count_errors++;
+		say "WARNING: nekorišteni key: $k";
+	}
+}
+
+# provjera nedostajucih KEYeva
+foreach my $k (keys %KEYS) {
+	my $v=$KEYS{$k};
+	if ($v =~ /([A-Z0-9_]{2,})/) {
+		$count_errors++;
+		say "ERROR: korištenje nepostojećeg keya u $k: $1";
+	}
+}
+
+exit $count_errors;
